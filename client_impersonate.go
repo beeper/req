175df4a5aa23ce843276560cf7d38c530e5c4113
Tulir Asokan <tulir@maunium.net>
2024-10-29 14:01:58 +0200

    Fix chrome headers

--- client_impersonate.go
+++ client_impersonate.go
@@ -105,17 +105,13 @@ var (
 	chromeHeaders = map[string]string{
 		"pragma":                    "no-cache",
 		"cache-control":             "no-cache",
-		"sec-ch-ua":                 `"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"`,
+		"sec-ch-ua":                 `"Not_A Brand";v="8", "Chromium";v="136", "Google Chrome";v="136"`,
 		"sec-ch-ua-mobile":          "?0",
-		"sec-ch-ua-platform":        `"macOS"`,
+		"sec-ch-ua-platform":        `"Linux"`,
 		"upgrade-insecure-requests": "1",
-		"user-agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+		"user-agent":                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
 		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-		"sec-fetch-site":            "none",
-		"sec-fetch-mode":            "navigate",
-		"sec-fetch-user":            "?1",
-		"sec-fetch-dest":            "document",
-		"accept-language":           "zh-CN,zh;q=0.9",
+		"accept-language":           "en-US,en;q=0.9",
 	}
 
 	chromeHeaderPriority = http2.PriorityParam{
