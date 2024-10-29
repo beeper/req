531c4e28460df9b3372e3368173a185c5dcf3944
Tulir Asokan <tulir@maunium.net>
2024-10-29 14:01:58 +0200

    Fix chrome headers

--- client_impersonate.go
+++ client_impersonate.go
@@ -105,17 +105,13 @@ var (
 	chromeHeaders = map[string]string{
 		"pragma":                    "no-cache",
 		"cache-control":             "no-cache",
-		"sec-ch-ua":                 `"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"`,
+		"sec-ch-ua":                 `"Not_A Brand";v="99", "Google Chrome";v="118", "Chromium";v="118"`,
 		"sec-ch-ua-mobile":          "?0",
-		"sec-ch-ua-platform":        `"macOS"`,
+		"sec-ch-ua-platform":        `"Linux"`,
 		"upgrade-insecure-requests": "1",
-		"user-agent":                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-		"sec-fetch-site":            "none",
-		"sec-fetch-mode":            "navigate",
-		"sec-fetch-user":            "?1",
-		"sec-fetch-dest":            "document",
-		"accept-language":           "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,it;q=0.6",
+		"user-agent":                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+		"accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+		"accept-language":           "en-US,en;q=0.9",
 	}
 
 	chromeHeaderPriority = http2.PriorityParam{
